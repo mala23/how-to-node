aa1f319b5336b9a83019dfebca9fda3e2feba4f0
mala23 <mail@mala23.me>
2014-10-27 12:14:29 +0000

    structured lessions

--- sample.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-console.log("Starting");
-fs.readFile("sample.txt", function(error, data) {
-    console.log("Contents of file: " + data);
-});
-console.log("Carry on executing");
