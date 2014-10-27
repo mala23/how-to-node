f2a6e545a57967f5b3c13443fc8c744c3baa063c
mala23 <mail@mala23.me>
2014-10-27 12:26:24 +0000

    read files synchronosly

--- lession03/read.js
+++ lession03/read.js
@@ -1,6 +1,5 @@
 var fs = require("fs");
 console.log("Starting");
-fs.readFile("./files/sample.txt", function(error, data) {
-  console.log("Contents: " + data);
-});
+var content = fs.readFileSync("./files/sample.txt");
+console.log("Contents: " + content);
 console.log("Carry on executing");
