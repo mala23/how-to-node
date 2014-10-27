7261b1f0f77e59254114bb0e825513ce1171ace9
mala23 <mail@mala23.me>
2014-10-27 12:33:47 +0000

    read and parse config.json

--- lession03/configuration.js
+++ lession03/configuration.js
@@ -2,3 +2,6 @@ var fs = require("fs");
 console.log("Starting");
 var contents = fs.readFileSync("./files/config.json");
 console.log("Contents: " + contents);
+var config = JSON.parse(contents);
+console.log("Config:", config);
+console.log("Username: ", config.username);
